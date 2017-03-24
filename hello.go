d7deac0622a52fae1512a580f715fffd64bc3130
davidalves1 <alves.david@outlook.com>
2017-03-24 00:10:43 -0300

    Atualização

--- hello.go
+++ hello.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Printf("Olá minha nova linguagem backend! s2")
+	// Define que é uma variável e seu tipo explicitamente
+	var s string
+	s = "Go"
+
+	// Define uma variável e seu tipo implicitamente utilizando o operador :=
+	name := "David"
+
+	// var b bool
+	// b = true
+
+	// var i int
+	// i = 42
+
+	// var f float32
+	// f = 2.93
+
+	fmt.Printf(s + " " + name)
+	// fmt.Printf(b)
+	// fmt.Printf(i)
+	// fmt.Printf(f)
 }
