189ebc671337861e6510c03ef620cd8eb56bc80c
davidalves1 <alves.david@outlook.com>
2017-03-24 00:49:54 -0300

    Operações matemáticas

--- hello.go
+++ hello.go
@@ -15,22 +15,24 @@ func main() {
 
 	// Define várias constantes ao mesmo tempo passando o tipo implicitamente
 	const (
-		saudacao = "Olá, tudo bem?\n"
+		saudacao = "Olá, tudo bem?"
 		idade = 30
 	)
 
 	// var b bool
 	// b = true
 
-	// var i int
-	// i = 42
-
 	// var f float32
 	// f = 2.93
 
-	fmt.Printf(saudacao)
-	fmt.Printf(s + " " + name + sobrenome)
-	// fmt.Printf(b)
-	// fmt.Printf(i)
-	// fmt.Printf(f)
+	fmt.Println(saudacao)
+	fmt.Println(s + " " + name + sobrenome)
+
+	a, b := 3.0, 2.0
+
+	fmt.Println(a + b)
+	fmt.Println(a * b)
+	fmt.Println(a - b)
+	fmt.Println(a / b)
+
 }
